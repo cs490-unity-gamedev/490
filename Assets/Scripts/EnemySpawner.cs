edbeb60676ccf98a0860572983e1f090d21ade59
Anh Dang <anhcdang@gmail.com>
2023-02-23 23:34:31 -0500

    adjust enemy spawning to be dynamic based on camera position

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private const int NORTH = 0;
+    private const int EAST = 1;
+    private const int SOUTH = 2;
+    private const int WEST = 3;
+
     [SerializeField]
     private GameObject enemyPrefab;
+    private List<Transform> spawnPoints = new List<Transform>();
+    private Transform northSpawnPoint;
+    private Transform eastSpawnPoint;
+    private Transform southSpawnPoint;
+    private Transform westSpawnPoint;
+
     [SerializeField]
     private float spawnInterval = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnPoints.Add(gameObject.transform.GetChild(0).gameObject.transform);
+        spawnPoints.Add(gameObject.transform.GetChild(1).gameObject.transform);
+        spawnPoints.Add(gameObject.transform.GetChild(2).gameObject.transform);
+        spawnPoints.Add(gameObject.transform.GetChild(3).gameObject.transform);
+
         StartCoroutine(spawnEnemy(spawnInterval, enemyPrefab));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy) {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        Transform spawnPoint = spawnPoints[Random.Range(NORTH,(WEST + 1))];
+        Vector3 spawnPointPosition = new Vector3(spawnPoint.position.x, spawnPoint.position.y, 0f);
+        Quaternion spawnPointRotation = spawnPoint.rotation;
+
+        GameObject newEnemy = Instantiate(enemy, spawnPointPosition, spawnPointRotation);
         
         // this ensures that it will be an endless game (can add counter to stop spawn after some time)
         StartCoroutine(spawnEnemy(interval, enemy));
