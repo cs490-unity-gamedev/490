4085563da634d511c0ef2091355c573f5f69f1e5
Anh Dang <anhcdang@gmail.com>
2023-03-15 17:58:16 -0400

    low-fi start and gameover ui + general game state logic script

--- Assets/Scripts/GameStateLogic.cs
+++ Assets/Scripts/GameStateLogic.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class GameStateLogic : MonoBehaviour
 {
     public void startGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // TODO: currently have hard-coded 1
+        SceneManager.LoadScene(1);
     }
 
     public void quitGame() {
         Application.Quit();
         Debug.Log("Player quit game.");
+    }
+
+    public void loadStartMenu() {
+        // TODO: currently have hard-coded 0
+        SceneManager.LoadScene(0);
     }
 }
