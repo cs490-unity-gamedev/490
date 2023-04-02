93806c40df2e80bb27de8951403b2715211f2e78
Anh Dang <anhcdang@gmail.com>
2023-04-02 17:20:23 -0400

    convert start menu to UI toolkit

--- Assets/Scripts/GameStateLogic.cs
+++ Assets/Scripts/GameStateLogic.cs
@@ -2,25 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class GameStateLogic : MonoBehaviour
 {
-    public void startGame() {
+    private void Awake() {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        root.Q<Button>("Play").clicked += () => startGame();
+        root.Q<Button>("Tutorial").clicked += () => loadTutorial();
+        root.Q<Button>("Quit").clicked += () => quitGame();
+    }
+    private void startGame() {
         // TODO: currently have hard-coded 1
         SceneManager.LoadScene(1);
     }
 
-    public void quitGame() {
+    private void quitGame() {
         Application.Quit();
         Debug.Log("Player quit game.");
     }
 
-    public void loadStartMenu() {
+    private void loadStartMenu() {
         // TODO: currently have hard-coded 0
         SceneManager.LoadScene(0);
     }
 
-    public void loadTutorial() {
+    private void loadTutorial() {
         SceneManager.LoadScene("TutorialScene1");
     }
 }
