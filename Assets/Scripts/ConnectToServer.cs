13bec5e95563e07cb63bc0021f5cfe04e9660863
David Katko <davidkatko@gmail.com>
2023-04-20 18:51:26 -0400

    remove oosless comment

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -5,7 +5,6 @@ using Photon.Pun;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
